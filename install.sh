ab8e0bb59309cdb760f7b3d3ccc94d31c84331f1
Werner Richter <werner.richter@cr14.net>
2025-02-07 00:23:51 +0100

    fix

--- install.sh
+++ install.sh
@@ -13,20 +13,19 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-
 # Add missing apps
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin qemu-guest-agent git-lfs \
  openconnect -y
-
 # Start docker service
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
-
 # Create workspace for codeserver
 mkdir ~/workspace
-
 # Install Code Server
 curl -fsSL https://code-server.dev/install.sh | sh
+cd /tmp
+curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.16.1.98d6fa.tar.gz
+tar -xf kasm_release_1.16.1.98d6fa.tar.gz
+sudo bash kasm_release/install.sh
